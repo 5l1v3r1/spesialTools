130c119ef4426c8b89b2e21795e3980e7bed20fe
BangDanz <52388234+BangDanz@users.noreply.github.com>
2020-01-29 00:32:52 +0700

    Add files via upload

---
+++ lord.sh
@@ -0,0 +1,98 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+endc='\E[0m'
+enda='\033[0m'
+echo
+echo $me"╭━━━━━━━╮"$cy "╔════════════════════════╗"
+echo $me"┃$pu ● ══$me  ┃"$cy "║$bi  Termux Tool Hacking$cy   ║"$me "██████████████"
+echo $me"┃$pu███████$me┃"$cy "║$i Gunakan Dengan Bijak$cy   ║"$me "██████████████"
+echo $me"┃$pu███████$me┃"$cy "║$me Author Tidak Bertang-$cy  ║"$pu "██████████████"
+echo $me"┃$pu███████$me┃"$cy "║$pu gung Jawab Atas Semua$cy  ║"$pu "██████████████"
+echo $me"┃$pu███████$me┃"$cy "║$pur Hal Yang Terjadi$cy       ║"
+echo $me"┃$pu███████$me┃"$cy "╚════════════════════════╝"$pu "INDONESIAN$me HACKER$pu RULES"
+echo $me"┃$pu███████$me┃"
+echo $me"┃$pu███████$me┃"$pur "╔════════════════════════╗"
+echo $me"┃$pu███████$me┃"$pur "║$pu Create : 28/01/2020$pur    ║"
+echo $me"┃$pu   ○$me   ┃"$pur "║$pu Author : MR.Apmz $pur       ║"
+echo $me"╰━━━━━━━╯"$pur "╚════════════════════════╝"
+echo
+sleep 3
+echo $me"╔════════════════════════╗"$cy "╔════════════════════════╗"
+echo $me"║$pu Sebelum login tools$me    ║"$cy "║$me Username & Password$cy    ║"
+echo $me"║$pu silahkan download user$me ║"$cy "║$me Tools lama sudah tidak$cy ║"
+echo $me"║$pu & pass terlebih dahulu$me ║"$cy "║$me berlaku / sudah di-$cy    ║"
+echo $me"║$bi Buat yang baru memakai$me ║"$cy "║$me update.$i Jadi, diharap-$cy ║"
+echo $me"║$bi harap instal bahan agar$me║"$cy "║$i kan untuk mendownload$cy  ║"
+echo $me"║$bi tidak terjadi error di-$me║"$cy "║$i Username & Password$cy    ║"
+echo $me"║$bi saat menjalankan tools$me ║"$cy "║$i yang baru. Thanks. >_<$cy ║"
+echo $me"╚════════════════════════╝"$cy "╚════════════════════════╝"
+sleep 2
+echo $pu"╔═════════════════════╗"
+echo $pu"║$i JANGAN LUPA SUBSCRIBE$pu ║"
+echo $pu"║$iYOUTUBE APMZ CHANNEL$pu   ║"
+echo $pu"╚═════════════════════╝"
+echo
+sleep 1
+python2 loading.py
+sleep 2
+echo $i"╔═══╗"$bi"╔═════════════╗"
+echo $i"║$pu 1$i ║"$bi"║$pu Login Tools$bi ║"
+echo $i"╚═══╝"$bi"╚═════════════╝"
+echo $i"╔═══╗"$bi"╔══════════════════════╗"
+echo $i"║$pu 2$i ║"$bi"║$pu Download User & Pass$bi ║"
+echo $i"╚═══╝"$bi"╚══════════════════════╝"
+echo $i"╔═══╗"$bi"╔══════════════════════╗"$i "╔════════════════════════╗"
+echo $i"║$pu 3$i ║"$bi"║$pu Download User & Pass$bi ║"$i "║$pu Jika Link diatas Error$i ║"
+echo $i"╚═══╝"$bi"╚══════════════════════╝"$i "╚════════════════════════╝"
+echo $i"╔═══╗"$bi"╔═══════════════╗"
+echo $i"║$pu 4$i ║"$bi"║$pu Install Bahan$bi ║"
+echo $i"╚═══╝"$bi"╚═══════════════╝"
+echo $i"╔═══╗"$bi"╔════════════════════════╗"
+echo $i"║$pu 5$i ║"$bi"║$pu    SUBSCRIBE APMZ CHANNEL$bi ║"
+echo $i"╚═══╝"$bi"╚════════════════════════╝"
+echo
+echo $bi"╔═══$pu Choose$bi ════"
+read -p "╚═══════════════➢➢ " pil
+if [ $pil = '1' ]
+then
+cd tool
+python2 run.py
+fi
+if [ $pil = '2' ]
+then
+echo $pu"OPEN YOUR BROWSER"
+sleep 3
+xdg-open https://adsafelink.com/MyWHcIn
+fi
+if [ $pil = '3' ]
+then
+echo $pu"OPEN YOUR BROWSER"
+sleep 3
+xdg-open https://sfile.mobi/2bp8v1BxBio
+fi
+if [ $pil = '4' ]
+then
+cd install
+python2 install.py
+fi
+if [ $pil = '5' ]
+then
+echo $i"TERIMAKASIH BANYAK TELAH MELUANGKAN WAKTU UNTUK SUBSCRIBE YT Apmz Channel"
+sleep 3
+echo $i"SILAHKAN BUKA YOUTUBE ANDA, LALU SUBSCRIBE  Apmz Channel"
+sleep 3
+xdg-open https://www.youtube.com/channel/UCS4EpzW-9kWTIt3Dmzb4oQQ
+fi
